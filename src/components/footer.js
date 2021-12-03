01ff90d087fefec5c6836f6644d406be66e16767
Vann <82990248+Stevanvincent@users.noreply.github.com>
2021-12-03 19:39:52 +0700

    add footer

---
+++ src/components/footer.js
@@ -0,0 +1,10 @@
+export default function Footer() {
+    return (
+        <footer className="mt-20">
+            <div className="h-full w-full text-gray-300 text-center font-outfit mb-3">
+                <h2>Made with 💗 by Vann-Dev</h2>
+                <p className="text-sm mt-2">Reference by <a href="https://rief.vercel.app/">Rifqi</a></p>
+            </div>
+        </footer>
+    )
+}
